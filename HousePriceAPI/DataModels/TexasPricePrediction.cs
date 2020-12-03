5ca850166eae315f7222877606ea1047f24e8b30
Stefan-Stev <stefanyo99@yahoo.com>
2020-12-03 20:10:43 +0200

    Second Turn
    Second Turn Melbourne Routing

---
+++ HousePriceAPI/DataModels/TexasPricePrediction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+namespace HousePriceAPI.DataModels
+{
+    public class TexasPricePrediction
+    {
+        public float Score { get; set; }
+    }
+}
