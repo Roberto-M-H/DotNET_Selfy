a85836953c18d872206ced81072a8e3244cadead
AdutP <61207523+AdutP@users.noreply.github.com>
2020-12-10 21:05:36 +0200

    Added Webassembly

---
+++ HousePriceAPI/Business/GetTexasHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using HousePriceAPI.DTOs;
+using HousePriceAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Linq;
+
+namespace HousePriceAPI.Business
+{
+    public class GetTexasHandler : IRequestHandler<GetTexas, List<Texa>>
+    {
+        private readonly Sims3Context context;
+        public GetTexasHandler(Sims3Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Texa>> Handle(GetTexas request, CancellationToken cancellationToken)
+        {
+            var fields = await this.context.Texas.OrderBy(h => h.PredictionId).Take(20).ToListAsync();
+            return fields;
+        }
+    }
+}
---
+++ HousePriceAPI/Data/Sims3Context.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+#nullable disable
+
+namespace HousePriceAPI.Data
+{
+    public partial class Sims3Context : DbContext
+    {
+        public Sims3Context()
+        {
+        }
+
+        public Sims3Context(DbContextOptions<Sims3Context> options)
+            : base(options)
+        {
+           
+        }
+
+        public virtual DbSet<Texa> Texas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Texa>(entity =>
+            {
+                entity.HasKey(e => e.PredictionId)
+                    .HasName("PK__Texas__F1AE77BF2DC6110A");
+
+                entity.Property(e => e.PredictionId).HasColumnName("prediction_id");
+
+                entity.Property(e => e.Bathrooms).HasColumnName("bathrooms");
+
+                entity.Property(e => e.Bedrooms).HasColumnName("bedrooms");
+
+                entity.Property(e => e.Condition).HasColumnName("condition");
+
+                entity.Property(e => e.Date)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("date");
+
+                entity.Property(e => e.Floors).HasColumnName("floors");
+
+                entity.Property(e => e.Grade).HasColumnName("grade");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Lat).HasColumnName("lat");
+
+                entity.Property(e => e.Long).HasColumnName("long");
+
+                entity.Property(e => e.Price).HasColumnName("price");
+
+                entity.Property(e => e.SqftAbove).HasColumnName("sqft_above");
+
+                entity.Property(e => e.SqftBasement).HasColumnName("sqft_basement");
+
+                entity.Property(e => e.SqftLiving).HasColumnName("sqft_living");
+
+                entity.Property(e => e.SqftLiving15).HasColumnName("sqft_living15");
+
+                entity.Property(e => e.SqftLot).HasColumnName("sqft_lot");
+
+                entity.Property(e => e.SqftLot15).HasColumnName("sqft_lot15");
+
+                entity.Property(e => e.View).HasColumnName("view");
+
+                entity.Property(e => e.Waterfront).HasColumnName("waterfront");
+
+                entity.Property(e => e.YrBuilt).HasColumnName("yr_built");
+
+                entity.Property(e => e.YrRenovated).HasColumnName("yr_renovated");
+
+                entity.Property(e => e.Zipcode).HasColumnName("zipcode");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
