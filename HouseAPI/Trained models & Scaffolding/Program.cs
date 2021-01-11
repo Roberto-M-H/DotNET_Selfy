a015909e3d847c565a82ee65cf2da43a951235ee
Andrei <gandrei.raileanu@gmail.com>
2021-01-11 11:51:00 +0200

    Added some trained models and the scaffolding code

---
+++ HouseAPI/Trained models & Scaffolding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
